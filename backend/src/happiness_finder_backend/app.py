8c089b77372c3773fc1fa86eb2a82e8d193bbb98
chickenta2ta <tishizuka1906@icloud.com>
2023-10-19 22:51:08 +0900

    fix: add cors

--- backend/src/happiness_finder_backend/app.py
+++ backend/src/happiness_finder_backend/app.py
@@ -1,16 +1,23 @@
+import base64
+import io
+
 from flask import Flask, request
+from flask_cors import CORS
 from PIL import Image
 from ultralytics import YOLO
 
 app = Flask(__name__)
+CORS(app)
 
 model = YOLO("model/best.pt")  # pretrained YOLOv8x model
 
 
 @app.route("/api/detect", methods=["POST"])
 def detect():
-    image = request.files["image"]
-    image = Image.open(image)
+    json_data = request.get_json()
+    image = json_data["image"].split(",")[1]
+    image = base64.b64decode(image)
+    image = Image.open(io.BytesIO(image))
 
     results = model(image)
     results = results[0]
